d61522be5c45a184f28e98e23f6e8eae8102b2f5
baijanathTharu <bnchaudhary.tharu@gmail.com>
2020-12-04 22:06:31 +0545

    add homescreen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className='App'>app</div>;
-}
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HomeScreen } from './screens/home.screen';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={HomeScreen}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/screens/home.screen.js
@@ -0,0 +1,12 @@
+import { Layout } from '../components/layout.component';
+
+export const HomeScreen = () => {
+  return (
+    <Layout>
+      <div
+        className=''
+        style={{ backgroundColor: 'wheat', height: '84.5vh' }}
+      ></div>
+    </Layout>
+  );
+};
