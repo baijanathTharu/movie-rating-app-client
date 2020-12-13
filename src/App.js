1a41bb1c7b6e25d940ec835e268c3a8a9e9db1e5
baijanathTharu <bnchaudhary.tharu@gmail.com>
2020-12-13 20:45:11 +0545

    add dashboard screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { useHistory } from 'react-router-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { HomeScreen, RegisterScreen } from './screens';
 import { DashboardScreen } from './screens';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const history = useHistory();
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) =>
+        localStorage.getItem('token') ? (
+          <Component {...routeProps} />
+        ) : (
+          history.push('/')
+        )
+      }
+    />
+  );
+};
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/register' component={RegisterScreen} />
-        <Route path='/dashboard' component={DashboardScreen} />
+        <ProtectedRoute path='/dashboard' component={DashboardScreen} />
         <Route path='/' exact component={HomeScreen} />
       </Switch>
     </BrowserRouter>
